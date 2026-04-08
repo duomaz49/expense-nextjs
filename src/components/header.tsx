ba1e23de254286647aa8251df257668a795b081a
Tuomas <tuomas49@gmail.com>
2026-04-08 15:42:16 +0300

    fix: welcome page is DONE

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="mb-12 flex justify-between items-center">
+      <h1 className="text-2xl font-bold text-black drop-shadow-lg">OpenFinance</h1>
+
+      <div className="flex gap-3">
+        <Button asChild>
+          <Link href="/auth/sign-in">
+            Sign In
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="/auth/sign-up">
+            Get Started
+          </Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
